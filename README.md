63ca94d629f47ccc95ae3a2daf9f9ab903c54b8b
passivestar <60579014+passivestar@users.noreply.github.com>
2024-09-26 17:17:00 +0400

    Add modern icons

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-![](./png/bake_icon.png)
-![](./png/texture_icon.png)
-![](./png/rename_icon.png)
-![](./png/object_index_icon.png)
-![](./png/hemi_icon.png)
+![](./output/light/bake_icon.png)
+![](./output/light/texture_icon.png)
+![](./output/light/rename_icon.png)
+![](./output/light/object_index_icon.png)
+![](./output/light/hemi_icon.png)
 
-# Flat ucupaint icons
+# Ucupaint icons
 
 ### Setup
 1. Get Python
-2. Get Inkscape
-3. Run `pip install -r requirements.txt`
+2. Run `pip install -r requirements.txt`
+
+Blender native icons can be explored via the official "Icon Viewer" addon and source files for them can be found in `release/datafile/icons_svg` folder of the Blender repository.
 
 ### Build process
-1. Export the icon sheet at 4096x4096px as `sheet.png` into the `svg` folder
+1. Export the icon sheets at 4096x4096px as `{light/dark/legacy}.png` into the `source/sheets` folder
 2. Run `python build.py`
--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+import os
 import json
 from PIL import Image
 
@@ -6,68 +7,79 @@ icons = json.load(open('./data/icons.json'))
 overlays = json.load(open('./data/overlays.json'))
 disabled = json.load(open('./data/disabled.json'))
 
-# Icon sheet
-sheet = Image.open('./svg/sheet.png')
+# Icon sheets
+sheet_files = ['light.png', 'dark.png', 'legacy.png']
+sheets = [(path, Image.open(f'./source/sheets/{path}')) for path in sheet_files]
+
 padding = 0.115
-vertical_correction = -10
-tile_size = sheet.width / 10
 
 # Tile extractor
-def crop_tile(index):
+def crop_tile(sheet, index):
+    tile_size = sheet[1].width / 10
     col = index % 10
     row = index // 10
 
-    top = row * tile_size + tile_size * padding + vertical_correction
+    top = row * tile_size + tile_size * padding
     left = col * tile_size + tile_size * padding
-    bottom = row * tile_size + tile_size - tile_size * padding + vertical_correction
+    bottom = row * tile_size + tile_size - tile_size * padding
     right = col * tile_size + tile_size - tile_size * padding
 
     coords = (left, top, right, bottom)
-    return sheet.crop(coords)
+    return sheet[1].crop(coords)
 
 # Tile downscale and save
-def save(tile, name):
+def save(tile, sheet, name):
     tile = tile.resize((32, 32), Image.Resampling.LANCZOS)
-    tile.save(f"./png/{name}.png")
+
+    sheet_base_name = sheet[0].replace('.png', '')
+
+    # Create directory in png folder if it doesn't exist
+    directory = f"./output/{sheet_base_name}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    tile.save(f"./output/{sheet_base_name}/{name}.png")
 
 # Regular
-def process_regular():
-    saveds = []
+def process_regular(sheet):
+    saved = []
     for i in range(len(icons)):
-        if icons[i] != '' and icons[i] not in saveds:
-            tile = crop_tile(i)
-            save(tile, icons[i])
-            saveds.append(icons[i])
+        if icons[i] != '' and icons[i] not in saved:
+            tile = crop_tile(sheet, i)
+            save(tile, sheet, icons[i])
+            saved.append(icons[i])
 
 # Overlays
-def process_overlays():
+def process_overlays(sheet):
     for o, (overlay_name, overlay_tiles) in enumerate(overlays.items()):
         # Read overlays from last row of the icon sheet
-        overlay = crop_tile(90 + o)
+        overlay = crop_tile(sheet, 90 + o)
 
         for i in overlay_tiles:
             if i >= len(icons):
                 print(f"Warning: Overlay \"{overlay_name}\" is defined for an image index that's out of icon name range ({i}). Skipping.")
                 continue
 
-            tile = crop_tile(i)
+            tile = crop_tile(sheet, i)
             tile.paste(overlay, (0, 0), overlay)
-            save(tile, f"{overlay_name}_{icons[i]}")
+            save(tile, sheet, f"{overlay_name}_{icons[i]}")
 
 # Disabled
-def process_disabled():
+def process_disabled(sheet):
     for d in range(len(disabled)):
         i = disabled[d]
         if i >= len(icons):
             print(f"Warning: Disabled icon definition is out of icon name range ({i}). Skipping.")
             continue
 
-        tile = crop_tile(i)
+        tile = crop_tile(sheet, i)
         alpha = tile.split()[-1]
         alpha = alpha.point(lambda x: int(x * 0.5))
         tile.putalpha(alpha)
-        save(tile, f"disabled_{icons[i]}")
+        save(tile, sheet, f"disabled_{icons[i]}")
 
-process_regular()
-process_overlays()
-process_disabled()
+for sheet in sheets:
+    print(f"Processing {sheet[0]}")
+    process_regular(sheet)
+    process_overlays(sheet)
+    process_disabled(sheet)
